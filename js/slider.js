428c89c5c34cea13b629ebf3a01a0301b6e40d51
Damianp99 <damianperez200199@gmail.com>
2022-01-27 17:50:40 +0100

    added some lines of code

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,46 @@
+/*
+Descrizione:
+Partendo dal markup fornito in allegato, implementare la logica per far funzionare lo slider:
+Deve essere possibile scorrere le immagini cliccando sulle freccette.
+I pallini in basso devono illuminarsi in base all'immagine che sto visualizzando.
+Far si che cliccando i pallini, venga selezionata l'immagine corrispondente.
+Note:  scegliamo una delle due seguenti soluzioni per le freccette:
+Se stiamo visualizzando la prima immagine, la freccetta di sinistra può:
+scomparire
+passare all'ultima immagine
+Se stiamo visualizzando l'ultima immagine, la freccetta  a destra può:
+scomparire
+passare alla prima immagine
+Siate coerenti: o scompaiono o continuano a navigare, no a soluzioni miste!!
+Bonus:
+1- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+2- quando il mouse va in hover sulle freccette o sui pallini l'autoplay si interrompe
+3 - l'autoplay riparte quando il mouse lascia le freccette o i pallini
+
+---------------
+Consigli del giorno:
+- regola d'oro: riciclare ovunque possibile! Questo significa che per la parte di markup possiamo far fare a Vue HTML ripetuto per noi.
+- il riciclo spesso va a braccetto coi methods! Sapendole sfruttare bene, l'esercizio si riduce a poche righe ;)
+- oltre a Vue, ora abbiamo diversi strumenti in più nelle nostre tasche, che possono tornarci utili per svolgere l'esercizio in una versione più evoluta ed efficace, soprattutto per quando riguarda la struttura dei dati.
+
+*/
+
 console.log('Vue ok', Vue);
 Vue.config.devtools = true
 
 const app = new Vue({
     el: '#root',
     data: {
+        currentIndex: 0,
+        images: [
+            'images/image1.jpg',
+            'images/image2.jpg',
+            'images/image3.jpg',
+            'images/image3.jpg',
+        ]
+    },
+    methods: {
 
-    }
+
+    },
 });
